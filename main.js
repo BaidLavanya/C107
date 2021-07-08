12b3c70f17e8564b2c9ed477b341e6c42e0e2172
BaidLavanya <75931821+BaidLavanya@users.noreply.github.com>
2021-07-08 13:49:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+Webcam.set ({
+    width: 350,
+    height:300,
+    image_format: 'png',
+    png_quality: 100
+});
+camera = document.getElementById("camera");
+Webcam.attach('#camera');
+
+function take_snap() {
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="captured_img" src="' + data_uri + '"/>';
+    });
+}
+
+console.log("ml5 version is " , ml5.version);
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/lZOeP7YiR/model.json",modelLoaded);
+
+function modelLoaded() {
+    console.log("Model Loaded!");
+}
